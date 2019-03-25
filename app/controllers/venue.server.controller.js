1de4367c767d212b7dfc149efa5dbf02ede14477
Ben <bad38@uclive.ac.nz>
2019-03-25 16:50:45 +1300

    get venue cat

--- app/controllers/venue.server.controller.js
+++ app/controllers/venue.server.controller.js
@@ -7,7 +7,18 @@ var path = require('path');
 exports.list = function (req, res) {
     Venue.getCats(function (result) {
         console.log(result);
+        out = [];
+        //console.log(result.length);
+        for (i = 0; i < result.length; i++) {
+            console.log(result[i].category_id);
+            out.push({
+                "categoryId": result[i].category_id,
+                "categoryName": result[i].category_name,
+                "categoryDescription": result[i].category_description
+            });
+        }
+
         res.status(200);
-        res.json(result);
+        res.json(out);
     })
 };
